12cd7d8fca6b61bb7976e5d4220e9f344d338694
Abderrahman Boukaa <aboukaa@gmail.com>
2021-09-08 19:05:22 +0200

    level-p

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -1,6 +1,6 @@
-result = []
 def fizzbuzz(n):
-    for x in range(1, n):
+    result = []
+    for x in range(1, n+1):
         if x % 15 == 0:
             result.append("fizzbuzz")
         elif x % 3 == 0:
@@ -8,5 +8,5 @@ def fizzbuzz(n):
         elif x % 5 == 0:
             result.append("buzz")
         else:
-            result.append(x)
+            result.append(int(x))
     return result
