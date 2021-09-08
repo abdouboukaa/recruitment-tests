5fe7b0143cbe3a37a56b4ef43429d7db8a32f3ac
abdouboukaa <aboukaa@gmail.com>
2021-09-08 11:42:45 +0200

    Update main.py

--- python/level-h/main.py
+++ python/level-h/main.py
@@ -1,2 +1,17 @@
-def compute_executable_projects(n, participants_votes, project_range):
-    pass
+def voteSystem(n, votes, project_range):
+    if project_range == 0:
+		return 'No projects, Sorry !'
+	projects={};
+	for i in range(0,n):
+		vote = votes[i]
+		for j in range(0,len(vote)):
+			if j in projects:
+				projects[j] = projects[j] + vote[j]
+			else:
+				projects[j] = vote[j]
+	orderedProjects = {k: v for k, v in sorted(projects.items(), key=lambda item: item[1], reverse = True)}
+	orderedProjectsKeys = [*orderedProjects.keys()]
+	output =[]
+	for i in range(0,project_range):
+		output.append(orderedProjectsKeys[i] + 1)
+	return output
