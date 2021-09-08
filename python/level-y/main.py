fd0cc87bc7fe8e455149e546436af63181f83c03
Abderrahman Boukaa <aboukaa@gmail.com>
2021-09-08 18:32:54 +0200

    level-y

--- python/level-y/main.py
+++ python/level-y/main.py
@@ -1,2 +1,10 @@
-def compute_temperature_data(temperature_data):
-    pass
+import statistics
+import numpy as np
+def compute_temperature_data(temps):
+    
+    average = statistics.mean(temps)
+    median = statistics.median(temps)
+    x = min(temps)
+    y = max(temps)
+    results = average,median,x,y
+    return np.round(results, 1)
